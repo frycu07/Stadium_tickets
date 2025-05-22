847c4bad258bfbd75d1a977be4ce7b1e8f6effc5
frycu07 <bartlomiej.fryc.7@gmail.com>
2025-05-22 19:56:41 +0200

    Initialize Spring Boot project with Maven wrapper and setup.
    Added Maven wrapper files and configurations for platform-specific builds. Configured `.gitignore` and `.gitattributes` for better project handling. Included essential dependencies, plugins, and the base application structure with a basic test setup.

---
+++ src/main/java/org/example/stadium_tickets/StadiumTicketsApplication.java
@@ -0,0 +1,13 @@
+package org.example.stadium_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadiumTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StadiumTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/stadium_tickets/StadiumTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stadium_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadiumTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
